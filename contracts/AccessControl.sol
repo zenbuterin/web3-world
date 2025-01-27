f7de90c8a9812c1248f73677d89aea14dab421e4
Guslizen Effayed <guslizenefayed@gmail.com>
2025-01-27 23:44:27 +0700

    menambahkan addmember, removemember, contructor function

--- contracts/AccessControl.sol
+++ contracts/AccessControl.sol
@@ -4,20 +4,33 @@ pragma solidity ^0.8.28;
 
 contract VotingManager {
 
+
     // enum yesOrNo { yes,no }
     // mapping (address => yesOrNo) options;
     mapping(address => uint) public voters;
     mapping(uint => Proposal) public createdProposal;
     mapping(address => mapping(uint => bool)) public hasVoted;
+    mapping(address => bool) public authority;
     uint public timesOver;
     uint private jumlahVoter = 0;
     uint public jumlahProposal;
     event getProposal(uint _nomor, address _proposer, uint _jumlah);
 
+    constructor() {
+        authority[msg.sender] = true;
+    }
     struct Proposal {
         string contentOfProposal;
         address proposer;
         uint jumlah;
+    }
+
+    function addMember(address _add) public {
+        authority[_add] = true;
+    }
+
+    function removeMember(address _add) public {
+        authority[_add] = false;
     }
 
     function createDeadLine(uint _periode) onlyAdmin public {
@@ -38,7 +51,7 @@ contract VotingManager {
         jumlahProposal++;
     }
 
-    function vote(address _voter, uint _nomorProposal) public {
+    function vote(address _voter, uint _nomorProposal, uint _jumlah) public {
         require(voters[_voter] <= jumlahVoter && hasVoted[_voter][_nomorProposal] == false, "kamu bukan voter teregistrasi ");
         require(block.timestamp <= timesOver, "Waktu Habis");
         createdProposal[_nomorProposal].jumlah += 1;
@@ -49,5 +62,10 @@ contract VotingManager {
         require(block.timestamp >= timesOver, "Waktu Habis");
         emit getProposal(_nomorProposal, createdProposal[_nomorProposal].proposer, createdProposal[_nomorProposal].jumlah);
     }
+
+    modifier onlyAdmin() {
+        require(authority[msg.sender] == true, "kamu bukan atmin");
+        _;
+    } 
 
 }
