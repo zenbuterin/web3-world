561a40bbef5895d2453d8e3d626977f83eb476fa
Guslizen Effayed <guslizenefayed@gmail.com>
2025-02-18 10:06:41 +0700

    memperbaiki beberapa kesalahan code pada smartContract

--- contracts/AccessControl.sol
+++ contracts/AccessControl.sol
@@ -1,48 +1,56 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-
 contract VotingManager {
-
-
-    // enum yesOrNo { yes,no }
-    // mapping (address => yesOrNo) options;
     mapping(address => uint) public voters;
     mapping(uint => Proposal) public createdProposal;
     mapping(address => mapping(uint => bool)) public hasVoted;
     mapping(address => bool) public authority;
     uint public timesOver;
-    uint private jumlahVoter = 0;
+    uint private jumlahVoter;
     uint public jumlahProposal;
+    
     event getProposal(uint _nomor, address _proposer, uint _jumlah);
+    event getKnowAuthority(bool _status);
+    event getKnowAdmin(address _addr);
+    event getHasVoted(bool _status);
 
     constructor() {
         authority[msg.sender] = true;
     }
+
     struct Proposal {
         string contentOfProposal;
         address proposer;
         uint jumlah;
     }
 
-    function addAdmin(address _add) onlyAdmin public {
-        authority[_add] = true;
+    function getAuthority(address _addr) public {
+        emit getKnowAuthority(authority[_addr]);
     }
 
-    function removeAdmin(address _add) onlyAdmin public {
-        authority[_add] = false;
+    function addAdmin(address _addr) public onlyAdmin {
+        require(voters[_addr] != 0, "Alamat ini bukan voter");
+        authority[_addr] = true;
+        emit getKnowAdmin(_addr);
     }
 
-    function createDeadLine(uint _periode) onlyAdmin public {
+    function removeAdmin(address _addr) public onlyAdmin {
+        require(voters[_addr] != 0, "Alamat ini bukan voter");
+        authority[_addr] = false;
+    }
+
+    function createDeadLine(uint _periode) public onlyAdmin {
         timesOver = block.timestamp + _periode;
     }
 
     function registration(address _voter) public {
-        voters[_voter] = jumlahVoter;
+        require(voters[_voter] == 0, "Voter sudah terdaftar");
         jumlahVoter++;
+        voters[_voter] = jumlahVoter; 
     }
 
-    function createProposal(string memory _content) onlyAdmin public {
+    function createProposal(string memory _content) public onlyAdmin {
         createdProposal[jumlahProposal] = Proposal({
             contentOfProposal: _content,
             proposer: msg.sender,
@@ -51,21 +59,32 @@ contract VotingManager {
         jumlahProposal++;
     }
 
-    function vote(address _voter, uint _nomorProposal) public {
-        require(voters[_voter] <= jumlahVoter && hasVoted[_voter][_nomorProposal] == false, "kamu bukan voter teregistrasi ");
+    function vote(uint _nomorProposal) public {
+        require(voters[msg.sender] != 0, "Anda bukan voter terdaftar");
+        require(_nomorProposal < jumlahProposal, "Proposal tidak ditemukan");
+        require(!hasVoted[msg.sender][_nomorProposal], "Anda sudah voting");
         require(block.timestamp <= timesOver, "Waktu Habis");
+
         createdProposal[_nomorProposal].jumlah += 1;
-        hasVoted[_voter][_nomorProposal] = true;
+        hasVoted[msg.sender][_nomorProposal] = true;
     }
 
     function getVoting(uint _nomorProposal) public {
-        require(block.timestamp >= timesOver, "Waktu Habis");
+        require(block.timestamp >= timesOver, "Voting masih berlangsung");
+        require(_nomorProposal < jumlahProposal, "Proposal tidak ditemukan");
+
         emit getProposal(_nomorProposal, createdProposal[_nomorProposal].proposer, createdProposal[_nomorProposal].jumlah);
     }
 
-    modifier onlyAdmin() {
-        require(authority[msg.sender] == true, "kamu bukan atmin");
-        _;
-    } 
+    function getKnowHasVoted(address _voter, uint _nomorProposal) public {
+        require(_nomorProposal < jumlahProposal, "Proposal tidak ditemukan");
 
+        bool hadVoted = hasVoted[_voter][_nomorProposal];
+        emit getHasVoted(hadVoted);
+    }
+
+    modifier onlyAdmin() {
+        require(authority[msg.sender], "Anda bukan admin");
+        _;
+    }
 }
