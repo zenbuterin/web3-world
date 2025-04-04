50a7373582a43dacbedee85450a91f343d85503a
Guslizen Effayed <guslizenefayed@gmail.com>
2025-04-04 18:30:32 +0700

    membuat tampilan header page utama

--- votingdapp/src/app/page.js
+++ votingdapp/src/app/page.js
@@ -8,6 +8,7 @@ function MyApp() {
   const [contract, setContract] = useState(null);
   const [account, setAccount] = useState(null);
   const [balanceethereum, setBalance] = useState(0);
+  const [dropDown, setDropDown] = useState(false);
 
 
   const connectToWeb3 = async () => {
@@ -30,13 +31,30 @@ function MyApp() {
 
   return (
     <>
-    <div className={styles.body}>
+    <div className={styles.innerBody}>
       <div className={styles.header}>
+        <div className={styles.logo}><h1>WEB3 WORLD</h1></div>
+        <div className={styles.overlapButton}>
         <button className={styles.navigator}>Dvoting</button>
         <button className={styles.navigator}>Swap Token</button>
         <button className={styles.navigator}>Wallet</button>
         <button className={styles.navigator}>Author</button>
         <button className={styles.walletconnection} onClick={connectToWeb3}>Connect Wallet</button>
+        </div>
+        <div className={styles.dropdownwrapper}>
+        <button className={styles.mobileView} onClick={() => {setDropDown(!dropDown); console.log("clicked")}}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+  <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
+</svg>
+        </button>
+        {dropDown && (<div className={styles.dropdownview}>
+        <button className={styles.navigator}>Dvoting</button>
+        <button className={styles.navigator}>Swap Token</button>
+        <button className={styles.navigator}>Wallet</button>
+        <button className={styles.navigator}>Author</button>
+        <button className={styles.walletconnection} onClick={connectToWeb3}>Connect Wallet</button>
+        </div>)}
+        </div>
       </div>
       <Dvoting />
     </div>
