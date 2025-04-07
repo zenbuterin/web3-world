1a9265b4233a14a964f385c8585b5010ab948b6c
Guslizen Effayed <guslizenefayed@gmail.com>
2025-04-07 10:38:06 +0700

    memisahkan setiap komponen, menggunakan shared layout untuk header + component

---
+++ votingdapp/src/app/dvoting/page.js
@@ -0,0 +1,19 @@
+import styles from "../page.module.css";
+
+export default function Dvoting() {
+    return(<>
+    <div className={styles.pageDvoting}>
+        <div className={styles.option}>
+            <div className={styles.info}>
+            <p>Roomname/roomnum</p>
+        </div>
+        <div className={styles.addNcandidate}>
+                <div><button className={styles.back}>Back</button></div>
+            </div>
+            </div>
+        <div className={styles.description}>
+        {/* photo dan description dari candidate */}
+        </div>
+    </div>
+    </>)
+    }
--- votingdapp/src/app/page.js
+++ votingdapp/src/app/page.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Web3 from "web3";
 import styles from "./page.module.css";
 import contractAbi from "./abi.json"
+import Dvoting from "./dvoting/page";
 function MyApp() {
   const [web3provider, setWeb3] = useState(null);
   const [contract, setContract] = useState(null);
@@ -28,39 +29,9 @@ function MyApp() {
       console.error("Error connecting to Web3:", error);
     }
   };
-
-  return (
-    <>
-    <div className={styles.innerBody}>
-      <div className={styles.header}>
-        <div className={styles.logo}><h1>WEB3 WORLD</h1></div>
-        <div className={styles.overlapButton}>
-        <button className={styles.navigator}>Dvoting</button>
-        <button className={styles.navigator}>Swap Token</button>
-        <button className={styles.navigator}>Wallet</button>
-        <button className={styles.navigator}>Author</button>
-        <button className={styles.walletconnection} onClick={connectToWeb3}>Connect Wallet</button>
-        </div>
-        <div className={styles.dropdownwrapper}>
-        <button className={styles.mobileView} onClick={() => {setDropDown(!dropDown); console.log("clicked")}}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-three-dots-vertical" viewBox="0 0 16 16">
-  <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
-</svg>
-        </button>
-        {dropDown && (<div className={styles.dropdownview}>
-          {/*ketika link dikunjungi maka componen akan muncul di div innerbody */}
-          <p className={styles.navdropdown}><a href="">Dvoting</a></p>
-          <p className={styles.navdropdown}><a href="">Swap Token</a></p>
-          <p className={styles.navdropdown}><a href="">Wallet</a></p>
-          <p className={styles.navdropdown}><a href="">Author</a></p>
-          <button className={styles.walletconnectiondropdown} onClick={connectToWeb3}><a href="">Connect to Wallet</a></button>
-        </div>)}
-        </div>
-      </div>
-      <Dvoting />
-    </div>
-    </>
-  );
+  return(<>
+  <p>ini kosong</p>
+  </>)
 }
 
 function GetBalance({ account, web3, setBalance, balanceethereum }) {
@@ -138,23 +109,6 @@ function GetKnowJumlahVoter({contract}) {
   )
 }
 
-function Dvoting() {
-  return(<>
-  <div className={styles.pageDvoting}>
-    <div className={styles.option}>
-      <div className={styles.info}>
-        <p>Roomname/roomnum</p>
-      </div>
-      <div className={styles.addNcandidate}>
-            <div><button className={styles.back}>Back</button></div>
-        </div>
-    </div>
-    <div className={styles.description}>
-      {/* photo dan description dari candidate */}
-    </div>
-  </div>
-  </>)
-}
 
 export default function Page() {
   return <MyApp />;
