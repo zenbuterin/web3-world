73be7198d2e1ef6c75f819073b026c09b654f585
Guslizen Effayed <guslizenefayed@gmail.com>
2025-05-20 20:14:57 +0700

    create votestate for votingroom (first version)

--- votingdapp/src/app/lib/voteState.js
+++ votingdapp/src/app/lib/voteState.js
@@ -1,3 +1,20 @@
-export default function useVoteState() {
-    
+'use client'
+import { createContext, useContext, useState } from "react";
+
+
+const VoteContext = createContext(null)
+export function VoteStateProvider({children}) {
+    const [roomcode, setRoomcode] = useState(0)
+    const [candidateaddress, setCandidateaddress] = useState("")
+    const [candidatecode, setCandidatecode] = useState(0)
+
+    return (
+        <VoteContext.Provider value={{roomcode, setRoomcode, candidateaddress, setCandidateaddress, candidatecode, setCandidatecode}}>
+            {children}
+        </VoteContext.Provider>
+    )
+}
+
+export function useVoteState() {
+    return useContext(VoteContext)
 }
