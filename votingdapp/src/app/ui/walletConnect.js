19eecd832cb7966f2ad30d7448a05933ca890e02
Guslizen Effayed <guslizenefayed@gmail.com>
2025-05-30 02:30:17 +0700

    fix seperate component with implement uicontext

--- votingdapp/src/app/ui/walletConnect.js
+++ votingdapp/src/app/ui/walletConnect.js
@@ -1,12 +1,13 @@
 'use client'
-import { captureOwnerStack } from "react";
-import { useWeb3State } from "../lib/web3state";
+import { useUIContext } from "../lib/UIContext";
+import { useWeb3State } from "../lib/web3stateContext";
 
 //NOTE: this component is button for connect to wallet installed on browser
 export default function ConnectToWallet() {
     const {address, createInstance} = useWeb3State()
+    const style = useUIContext()
     
     return (
-        <button onClick={()=> {createInstance()}}>{address ? address : "Connect wallet"}</button>
+        <button onClick={()=> {createInstance()}} className={style.walletconnection}>{address ? address : "Connect wallet"}</button>
         )
 }
