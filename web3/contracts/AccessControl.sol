9c3015df775adec4489423ec30fa7fe3632661f2
Guslizen Effayed <guslizenefayed@gmail.com>
2025-06-16 03:57:52 +0700

    add restriction and getter function

--- web3/contracts/AccessControl.sol
+++ web3/contracts/AccessControl.sol
@@ -2,9 +2,18 @@
 pragma solidity ^0.8.28;
 
 contract VotingManager {
-    event proposalCreated(uint indexed proposalId);
-    event optionAdded(uint indexed proposalId, uint indexed optionId);
-    event voted(uint indexed proposalId, uint indexed optionId, uint voteCount);
+    event proposalCreated(address createdBy, uint indexed proposalId);
+    event optionAdded(address addedBy, uint indexed proposalId, uint indexed optionId);
+    event voted(address voter, uint indexed proposalId, uint indexed optionId, uint voteCount);
+
+    uint private proposalCount;
+    mapping(address => bool) public authorized;
+    mapping(uint => Proposal) private proposals;
+    mapping(uint => mapping(address => bool)) public isVoted; 
+
+    constructor() {
+        authorized[msg.sender] = true;
+    }
 
     struct Option {
         uint id;
@@ -17,26 +26,34 @@ contract VotingManager {
         uint optionCount;
     }
 
-    uint private proposalCount;
-    mapping(uint => Proposal) private proposals;
-
-    function createdProposal() public {
+    function createProposal() public {
+        require(authorized[msg.sender] == true, "You are not authorized");
         proposalCount++;
         Proposal storage proposal = proposals[proposalCount];
         proposal.id = proposalCount;
-        emit proposalCreated(proposalCount);
+        emit proposalCreated(msg.sender, proposalCount);
     }
 
     function addOption(uint proposalId) public {
+        require(authorized[msg.sender] == true, "You are not authorized");
         Proposal storage proposal = proposals[proposalId];
         proposal.optionCount++;
         proposal.options[proposal.optionCount] = Option(proposal.optionCount, 0);
-        emit optionAdded(proposalId, proposal.optionCount);
+        emit optionAdded(msg.sender, proposalId, proposal.optionCount);
     }
 
     function vote(uint proposalId, uint optionId) public {
+        require(!isVoted[proposalId][msg.sender], "you have voted");
+        require(proposalId > 0 && proposalId <= proposalCount, "Invalid proposal");
         Proposal storage proposal = proposals[proposalId];
+        require(optionId > 0 && optionId <= proposal.optionCount, "Invalid option");
         proposal.options[optionId].voteCount++;
-        emit voted(proposalId, proposal.optionCount, proposal.options[optionId].voteCount);
+        isVoted[proposalId][msg.sender] = true;
+        emit voted(msg.sender, proposalId, optionId, proposal.options[optionId].voteCount);
+    }
+
+    function getvoteCount(uint proposalId, uint optionId) public view returns (uint) {
+        Proposal storage proposal = proposals[proposalId];
+        return proposal.options[optionId].voteCount;
     }
 }
