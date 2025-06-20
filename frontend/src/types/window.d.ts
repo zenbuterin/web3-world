7452e396a96652c3732b1e7d9a330697bdab3821
Guslizen Effayed <guslizenefayed@gmail.com>
2025-06-20 12:04:00 +0700

    types for interaction (can be adjust)

--- frontend/src/types/window.d.ts
+++ frontend/src/types/window.d.ts
@@ -1,5 +1,5 @@
 import type { MetaMaskInpageProvider } from '@metamask/providers';
-
+// Window Ethereum Declaration
 declare global {
     interface Window {
         ethereum?: MetaMaskInpageProvider;
