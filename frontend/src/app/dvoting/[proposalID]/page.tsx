0b5a856bb00c3edbc5068e86c77d360258036f60
Guslizen Effayed <guslizenefayed@gmail.com>
2025-06-22 19:56:53 +0700

    adding doc for current feature

--- frontend/src/app/dvoting/[proposalID]/page.tsx
+++ frontend/src/app/dvoting/[proposalID]/page.tsx
@@ -1,20 +1,11 @@
 'use state'
-import useVote from "@/context/voteFunctionalities";
 import styles from "@/style/dvoting.module.css";
-import VoteTheCandidate from "@/components/dvoting/votingroom/votingbutton";
 //NOTE: voting room will take data from state if state is created from createRoom component
 //NOTE: it takes data from smartcontract
 export default function page() {
     return(<>
     <div className={styles.pageDvoting}>
-        <VoteTheCandidate />
-        <div className={styles.description}>
-            <div className={styles.candidateBox}>
-                <div className={styles.candidate}></div>
-                <div className={styles.candidate}></div>
-            </div>
-            <div className={styles.dataVisual}></div>
-        </div>
+        {/*TODO: this page used for display all information about related proposal */}
     </div>
     </>)
     }
