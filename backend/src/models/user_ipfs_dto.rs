387f81d6dc682219596e889e5ddb5f98c22b16de
Guslizen Effayed <guslizenefayed@gmail.com>
2025-07-05 14:36:11 +0700

    adjust models

--- backend/src/models/user_ipfs_dto.rs
+++ backend/src/models/user_ipfs_dto.rs
@@ -1,23 +1,30 @@
 use serde::{Serialize, Deserialize};
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct IpfsResponse {
+    pub hash: String,
+    pub success: bool,
+    pub message: String,
+}
 
-#[derive(Serialize, Deserialize)]
-pub struct Proposals {
-   pub proposal: Vec<Proposal>
+#[derive(Debug, Serialize, Deserialize)]
+pub struct JsonData {
+    pub id: String,
+    pub name: String,
+    pub data: serde_json::Value,
+    pub timestamp: i64,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct StoreRequest {
+    pub data: serde_json::value,
+    pub pin: Option<bool>
 }
 
 
-#[derive(Serialize, Deserialize)]
-pub struct Proposal {
-    pub id: u64,
-    pub title: String,
-    pub description: String,
-    pub options: Vec<ProposalOption>
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RetrieveRequest {
+    pub hash: String
 }
 
 
-#[derive(Serialize, Deserialize)]
-struct ProposalOption {
-    pub id: u64,
-    pub description: String,
-}
